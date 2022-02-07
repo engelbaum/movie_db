867eef291f0ee3c8e3225211e28ed110b15cd879
jawad <eliarok@hotmail.com>
2022-02-07 20:30:18 +0100

    movie

--- connexion.php
+++ connexion.php
@@ -1,3 +1,10 @@
 <?php
-try {     $dbh = new PDO('mysql:host=localhost;dbname=test', $user, $pass);     $dbh = null; } 
-catch (PDOException $e) {     print "Error!: " . $e->getMessage() . "<br/>";     die(); }
+$user = 'root';
+$pass = '';
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=movie_db', $user, $pass);
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
